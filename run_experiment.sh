31212262774de6629f19c555e0dcc33b48cbccdf
Evžen Wybitul <wybitul.evzen@gmail.com>
2024-01-31 23:18:31 +0000

    Update run script

--- run_experiment.sh
+++ run_experiment.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+#SBATCH --nodes=1
+#SBATCH --gpus=1
+#SBATCH --time=04:00:00
+#SBATCH --partition=single
+#SBATCH --job-name=vlmrm
+#SBATCH --output=logs/job_%j.log
+
+mkdir -p logs
+
 echo "Make sure you have WANDB_API_KEY set in .env"
-source ".env"
+export $(cat .env | xargs)
 
 vlmrm train "$(cat config.yaml)"
